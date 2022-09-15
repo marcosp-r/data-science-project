b811e401697342ded2d9118fe7b928487922ffd3
Marcos Padilla-Ruiz <marcos@Marcoss-MacBook-Air.local>
2022-09-15 15:44:08 -0700

    tropical plant traits activity

--- data/Tropical-plant-trait.R
+++ data/Tropical-plant-trait.R
@@ -31,20 +31,27 @@ fruit_width <- data_table[, 7]
 mean(data_table$fruit_width, na.rm=TRUE)
 summary(data_table$fruit_width)
 
+
 firstactivity <- data_table[1:10, ]
+write.csv(firstactivity)
 
 secondactivity <- data_table[137:157, ]
+write.csv(secondactivity)
 
 thirdactivity <- data_table[c(12, 17, 23, 145),]
+write.csv(thirdactivity)
 
 fourthactivity <- data_table[c(1:10, 12, 17, 23, 145), ]
+write.csv(fourthactivity)
 
 fifthactivity <- data_table[ , 1:5]
+write.csv(fifthactivity)
 
 sixthactivity <- data_table[ , 12:14]
+write.csv(sixthactivity)
 
 seventhactivity <- data_table[, c(3, 5, 13)]
+write.csv(seventhactivity)
 
 eighthactivity <- data_table[c(12, 17, 23, 145), 1:5]
-
-
+write.csv(eighthactivity)
